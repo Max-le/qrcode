7dc346df468c242ffab10c1866f27f1c5ec1bd09
Max <max.09@outlook.com>
2018-10-27 21:07:57 +0200

    add files

---
+++ qr.py
@@ -0,0 +1,16 @@
+
+
+# Documentation : https://pypi.org/project/qrcode/
+
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qr.add_data('HELLO WORLD !!! 🥙')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="transparent")
+img.show()
