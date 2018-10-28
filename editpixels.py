74571f085808119681e94119bc80060da9b24439
Max <max.09@outlook.com>
2018-10-28 11:35:29 +0100

    a code to change the color of the pixels

---
+++ editpixels.py
@@ -0,0 +1,24 @@
+# Source of this code : https://en.wikibooks.org/wiki/Python_Imaging_Library/Editing_Pixels
+
+
+from PIL import Image
+
+# PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
+# img = Image.new( 'RGB', (250,250), "black") # create a new black image
+
+img = Image.open('test.png')
+
+pixels = img.load() # create the pixel map
+
+
+
+
+for i in range(img.size[0]):    # for every col:
+    for j in range(img.size[1]):    # For every row
+        if pixels[i,j] == (0, 0, 0, 255):
+            print("BLACK PIXEL ! ")
+            pixels[i,j] = (i, j, 100) # set the colour accordingly
+
+        #print(pixels[i,j])
+
+img.show()
