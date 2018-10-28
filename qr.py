b79d4c0bfcbc6db47fa1ff76aa2aaa541cb04a21
Max <max.09@outlook.com>
2018-10-28 11:14:12 +0100

    more testing

--- qr.py
+++ qr.py
@@ -3,14 +3,18 @@
 # Documentation : https://pypi.org/project/qrcode/
 
 import qrcode
+from PIL import ImageOps
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
     border=4,
 )
-qr.add_data('WIFI:T:WPA;S:mynetwork;P:mypass;;')
+qr.add_data('http://qonnect.ml')
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="transparent")
+img.show()
+img.save('test.png')
+img = ImageOps.invert(img)
 img.show()
